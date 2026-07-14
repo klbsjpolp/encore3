33ac889ae57a3bc72b908ca9d638487a7350966d
plcharette <plcharette@360-innovations.com>
2026-07-14 14:09:05 -0400

    feat(a11y): add keyboard hint component for spacebar actions

---
+++ src/components/game/KeyboardHint.tsx
@@ -0,0 +1,7 @@
+export const KeyboardHint = () => {
+  return (
+    <kbd className="pointer-events-none hidden rounded border border-current px-1 font-mono text-[10px] font-normal opacity-75 lg:inline-block">
+      Espace
+    </kbd>
+  )
+}
