26fe466d4b478fc5076d589e32f15edc5516dc0a
plcharette <plcharette@360-innovations.com>
2026-07-14 16:23:06 -0400

    fix(a11y): add aria-hidden attribute to keyboard hint component

--- src/components/game/KeyboardHint.tsx
+++ src/components/game/KeyboardHint.tsx
@@ -1,6 +1,9 @@
 export const KeyboardHint = () => {
   return (
-    <kbd className="pointer-events-none hidden rounded border border-current px-1 font-mono text-[10px] font-normal opacity-75 lg:inline-block">
+    <kbd
+      aria-hidden={true}
+      className="pointer-events-none hidden rounded border border-current px-1 font-mono text-[10px] font-normal opacity-75 lg:inline-block"
+    >
       Espace
     </kbd>
   )
